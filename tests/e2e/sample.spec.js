94a84cf525cb383a2433b5029067845ef9f1248f
Praveen Kumar C <praveen.c@jseven.in>
2026-05-18 15:08:31 +0530

    setup playwright CI

---
+++ tests/e2e/sample.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('homepage loads correctly', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page).toHaveTitle(/frontend/);
+});
